69e629a8b62700e5144bf60707b47e4a13ddacfa
Walker Dunn-O'Connor <walkerj.dunn@gmail.com>
2017-12-18 01:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
   for(let i=0; i < cart.length; i++){
     var item = Object.keys(cart[i])[0]
     var price = cart[i][item]
-    allItemsAndPrices.push(parseInt(price))
+    prices.push(parseInt(price))
   }
   function getSum(total,num){
     return total + num;
