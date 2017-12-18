e2124212c2cf6a154d7f86c226622488b8e5849b
Walker Dunn-O'Connor <walkerj.dunn@gmail.com>
2017-12-18 01:07:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
     console.log("Your shopping cart is empty.")
   }
   else if (cart.length = 1) {
-    console.log(`In your cart, you have ${item} at ${price}`)
+    console.log(`In your cart, you have ${item} at ${price}.`)
   }{
     return "come back to me"
   }
