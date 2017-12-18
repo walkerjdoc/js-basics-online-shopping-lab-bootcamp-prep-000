9addc3fedfa66066b2b8948573a294842084789d
Walker Dunn-O'Connor <walkerj.dunn@gmail.com>
2017-12-18 02:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,12 +60,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber == undefined){
-    console.log(`We don't have a credit card on file for you.`)
+    console.log(`Sorry, we don't have a credit card on file for you.`)
     return cart
 }
   else{
-    console.log(`Your total cost is$${total([cart])}, which will be charged to the card ${cardNumber}.`)
-  cart.length = 0
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.length = 0
     return cart
   }
 }
